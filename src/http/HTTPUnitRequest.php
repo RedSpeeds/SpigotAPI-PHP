4cf9c35db81589a40c04b9be9a97b3953eacc762
TheCreeperCow <thecreepercow@gmail.com>
2018-03-15 10:56:22 +0100

    This seems impossible

--- src/http/HTTPUnitRequest.php
+++ src/http/HTTPUnitRequest.php
@@ -13,16 +13,32 @@ require __DIR__ . '/../../vendor/autoload.php';
 
 class HTTPUnitRequest
 {
-    public $webClient= null;
+    public $webClient= new Curl();
+    public $baseurl = "https://spigotmc.org/";
     const useragent = "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36";
     public function init(){
-        $this->$webClient = new Curl();
-        $webClient->setOpt("CURLOPT_CONNECTTIMEOUT_MS",15000);
-        $webClient->setOpt("CURLOPT_FOLLOWLOCATION",true);
-        $webClient->setUserAgent(self::useragent);
+        try {
+            $this->webClient = new Curl();
+        } catch (\ErrorException $e) {
+        }
+        $this->webClient->setOpt("CURLOPT_CONNECTTIMEOUT_MS",15000);
+        $this->webClient->setOpt("CURLOPT_FOLLOWLOCATION",true);
+        $this->webClient->setUserAgent(self::useragent);
     }
-    public static function get($url,array $cookies,array $params){
-        we
+    public function get($url,array $cookies,array $params){
+        foreach ($cookies as $key => $value){
+            $this->webClient->setCookie($key, $value);
+        }
+        $parmsurl = "";
+        foreach ($params as $key => $value){
+            $parmsurl.="?".$key."=".$value."&";
+        }
+        $this->webClient->setUrl($this->baseurl.$url.$parmsurl);
+        $result = $this->webClient->exec();
+        $page = new \DOMDocument();
+        $page->loadHTML($result);
+
+
     }
 
 }
